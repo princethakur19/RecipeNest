aef8bd9b175a457cb9929ff5e00e9be41dba8cab
princethakur19 <princethakur7778@gmail.com>
2026-07-25 16:21:01 +0530

    Update recipe model to use Array for ingredients and enhance AddFoodRecipe component with form handling

--- frontend/recipe-app/src/pages/AddFoodRecipe.jsx
+++ frontend/recipe-app/src/pages/AddFoodRecipe.jsx
@@ -1,25 +1,41 @@
-
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const AddFoodRecipe = () => {
+
+    const [recipeData, setRecipeData] = useState({})
+    const navigate = useNavigate()
+    const onHandleChange = (e)=>{
+        let val = (e.target.name === "ingredients") ? e.target.value.split(",") : e.target.value
+        setRecipeData(pre => ({...pre, [e.target.name]:val}))
+    }
+
+    const onHandleSubmit = async (e)=>{
+        e.preventDefault()
+        console.log(recipeData)
+        await axios.post("http://localhost:5000/recipe", recipeData)
+        .then(()=>navigate("/"))
+    }
   return (
     <>
       <div className="container">
-        <form className="form">
+        <form className="form" onSubmit={onHandleSubmit}>
             <div className="form-control">
                 <label>Title</label>
-                <input type="text" className="input" name="title"/>
+                <input type="text" className="input" name="title" onChange={onHandleChange}/>
             </div>
             <div className="form-control">
                 <label>Time</label>
-                <input type="text" className="input" name="time"/>
+                <input type="text" className="input" name="time" onChange={onHandleChange}/>
             </div>
             <div className="form-control">
                 <label>Ingredients</label>
-                <textarea type="text" className="input" name="ingredients" rows="5"/>
+                <textarea type="text" className="input" name="ingredients" rows="5" onChange={onHandleChange}/>
             </div>
             <div className="form-control">
                 <label>Intructions</label>
-                <textarea type="text" className="input" name="intsructions" rows="5"/>
+                <textarea type="text" className="input" name="intsructions" rows="5" onChange={onHandleChange}/>
             </div>
             <div className="form-control">
                 <label>Recipe Image</label>
