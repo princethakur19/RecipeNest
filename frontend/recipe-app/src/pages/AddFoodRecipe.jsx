f78c331e7ff201d98bada292191a0ce41100da1e
princethakur19 <princethakur7778@gmail.com>
2026-07-25 15:47:08 +0530

    create form in AddFoodRecipe.jsx

--- frontend/recipe-app/src/pages/AddFoodRecipe.jsx
+++ frontend/recipe-app/src/pages/AddFoodRecipe.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+
 
 const AddFoodRecipe = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container">
+        <form className="form">
+            <div className="form-control">
+                <label>Title</label>
+                <input type="text" className="input" name="title"/>
+            </div>
+            <div className="form-control">
+                <label>Time</label>
+                <input type="text" className="input" name="time"/>
+            </div>
+            <div className="form-control">
+                <label>Ingredients</label>
+                <textarea type="text" className="input" name="ingredients" rows="5"/>
+            </div>
+            <div className="form-control">
+                <label>Intructions</label>
+                <textarea type="text" className="input" name="intsructions" rows="5"/>
+            </div>
+            <div className="form-control">
+                <label>Recipe Image</label>
+                <textarea type="file" className="input" name="file" />
+            </div>
+            <button type="submit">Add Recipe</button>
+        </form>
+      </div>
+    </>
   )
 }
 
