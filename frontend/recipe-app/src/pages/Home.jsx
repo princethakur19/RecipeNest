349fbda3dd12c4c653e050839344aab03ba610e0
princethakur19 <princethakur7778@gmail.com>
2026-07-29 21:54:21 +0530

    add method for add recipe and also create state

--- frontend/recipe-app/src/pages/Home.jsx
+++ frontend/recipe-app/src/pages/Home.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import foodRecipe from '../assets/foodRecipe.png'
 import RecipeItems from '../components/RecipeItems'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 
 function Home() {
   const navigate = useNavigate()
+  const [isOpen, setIsOpen] = useState(false)
+  const addRecipe=()=>{
+    navigate("/addRecipe")
+  }
   return (
     <>
       <section className='home'>
         <div className='left'>
             <h1>Food Recipe</h1>
             <h5>Good food lives here. Explore simple, mouthwatering recipes made with everyday ingredients. From quick weeknight dinners to sweet treats, we have your next meal covered.</h5>
-            <button onClick={()=>navigate("/addRecipe")}>Share your recipe</button>
+            <button onClick={addRecipe}>Share your recipe</button>
         </div>
         <div className='right'>
             <img src={foodRecipe} width="320px" height="300px" />
