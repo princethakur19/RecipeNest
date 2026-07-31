3aa8477356c1840046756958aa7f0611e4e1286a
princethakur19 <princethakur7778@gmail.com>
2026-07-31 22:31:49 +0530

    get the user id from localhost

--- frontend/recipe-app/src/App.jsx
+++ frontend/recipe-app/src/App.jsx
@@ -13,8 +13,10 @@ const getAllRecipe = async()=>{
   return allRecipe
 }
 
-const getAllRecipe = async()=>{
+const getMyRecipe = async()=>{
   let user = JSON.parse(localStorage.getItem(user))
+  let allRecipe = await getAllRecipe()
+  return allRecipe.filter(item=>item.createdBy === user._id)
 }
 
 const router = createBrowserRouter([
