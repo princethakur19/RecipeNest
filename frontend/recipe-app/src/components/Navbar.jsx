2feb48c03c5cea77e019768d436ec7aa54328bc4
princethakur19 <princethakur7778@gmail.com>
2026-08-01 22:18:55 +0530

    push commit(error)

--- frontend/recipe-app/src/components/Navbar.jsx
+++ frontend/recipe-app/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
   const [isOpen,setIsOpen] = useState(false)
   let token = localStorage.getItem("token")
   const [isLogin, setIsLogin] = useState(token ? false : true)
+  let user = JSON.parse(localStorage.getItem(user))
 
   useEffect(()=>{
     setIsLogin(token ? false : true)
@@ -32,7 +33,7 @@ function Navbar() {
             <li><NavLink to="/">Home</NavLink></li>
             <li onClick={()=>isLogin && setIsOpen(true)}><NavLink to={!isLogin ? "/myRecipe" : "/"}>My Recipe</NavLink></li>
             <li onClick={()=>isLogin && setIsOpen(true)}><NavLink to={!isLogin ? "/favRecipe" : "/"}>Favourites</NavLink></li>
-            <li onClick={checkLogin}><p className="login">{(isLogin)? "Login": "Logout"}</p></li>
+            <li onClick={checkLogin}><p className="login">{(isLogin)? "Login": "Logout"}{user?.email ? user?.email : ""}</p></li>
         </ul>
       </header>
       { (isOpen) && <Model onClose={()=> setIsOpen(false)}><InputForm setIsOpen={()=>setIsOpen(false)} /></Model>}
