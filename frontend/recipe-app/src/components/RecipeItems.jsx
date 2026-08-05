0a1c624786914c16e6727253f61ec39d8263b24c
princethakur19 <princethakur7778@gmail.com>
2026-08-05 22:45:14 +0530

    fix: correct conditional rendering for heart icon in RecipeItems component

--- frontend/recipe-app/src/components/RecipeItems.jsx
+++ frontend/recipe-app/src/components/RecipeItems.jsx
@@ -22,11 +22,11 @@ function RecipeItems() {
                             <div className="title">{item.title}</div>
                             <div className="icons">
                                 <div className="timmer"><BsFillStopwatchFill />30min</div>
-                                {(!path)}<FaHeart />
+                                {(!path) ? <FaHeart /> :
                                 <div className="action">
                                     <FaEdit />
                                     <MdDelete className='deleteIcon' />
-                                </div>  
+                                </div>  }
                             </div>
                         </div>
                     </div>
